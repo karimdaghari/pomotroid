46c2c38a2335c3b68bef03e6aec4426bb2579c53
Karim Daghari <karimdxy@gmail.com>
2019-08-13 16:10:55 +0100

    setSize works only once

--- src/renderer/components/Titlebar.vue
+++ src/renderer/components/Titlebar.vue
@@ -57,23 +57,45 @@
         style="padding-right: 5px"
         @click="winMiniMode"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke="#F6F2EB"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="Icon Icon--miniMode"
-        >
-          <polyline points="4 14 10 14 10 20" />
-          <polyline points="20 10 14 10 14 4" />
-          <line stroke="#F6F2EB" x1="14" y1="10" x2="21" y2="3" />
-          <line stroke="#F6F2EB" x1="3" y1="21" x2="10" y2="14" />
-        </svg>
+        <template v-if="this.miniMode">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="#F6F2EB"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="Icon Icon--miniMode"
+          >
+            <polyline points="4 14 10 14 10 20" />
+            <polyline points="20 10 14 10 14 4" />
+            <line stroke="#F6F2EB" x1="14" y1="10" x2="21" y2="3" />
+            <line stroke="#F6F2EB" x1="3" y1="21" x2="10" y2="14" />
+          </svg>
+        </template>
+        <template v-else>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#F6F2EB"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="Icon Icon--minimize"
+          >
+            <polyline points="15 3 21 3 21 9" />
+            <polyline points="9 21 3 21 3 15" />
+            <line x1="21" y1="3" x2="14" y2="10" />
+            <line x1="3" y1="21" x2="10" y2="14" />
+          </svg>
+        </template>
       </div>
+
       <div
         class="Icon-wrapper Icon-wrapper--titlebar Icon-wrapper--double--right"
         style="padding-right: 18px"
